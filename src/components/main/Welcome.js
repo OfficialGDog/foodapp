612c7d3e4c1e7ff941a2dd12236cef7f80ad80a5
Gareth Davies <garethdavieslive@gmail.com>
2021-03-31 18:30:01 +0100

    Added real-time data rendering for categories / dietary needs from firestore database.

--- src/components/main/Welcome.js
+++ src/components/main/Welcome.js
@@ -18,6 +18,9 @@ import "./Welcome.css";
 
 export default function Welcome() {
   let [mySwiper, setMySwiper] = useState(null);
+  const [categories, setCategories] = useState([]);
+  const [dietaryConditions, setDietaryConditions] = useState([]);
+  const [foods, setFoods] = useState([]);
   const history = useHistory();
 
   Swiper.use([Navigation, Pagination]);
@@ -39,116 +42,85 @@ export default function Welcome() {
     });
 
     setMySwiper(swiper);
+    setCategories([]);
+    setDietaryConditions([]);
+    setFoods([]);
 
-    fireTest();
+    let unsubscribe1 = firestore.collection("foods").onSnapshot(
+      (snapshot) => {
+        snapshot.docChanges().forEach((change) =>
+          updateState({
+            foods: {
+              data: [{ ...change.doc.data(), path: change.doc.ref.path }],
+            },
+          })
+        );
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
 
+    let unsubscribe2 = firestore.collection("categories").onSnapshot(
+      (snapshot) => {
+        snapshot.docChanges().forEach((change) =>
+          updateState({
+            categories: {
+              data: [{ ...change.doc.data(), path: change.doc.ref.path }],
+            },
+          })
+        );
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+    let unsubscribe3 = firestore.collection("dietaryconditions").onSnapshot(
+      (snapshot) => {
+        snapshot.docChanges().forEach((change) =>
+          updateState({
+            dietaryconditions: {
+              data: [{ ...change.doc.data(), path: change.doc.ref.path }],
+            },
+          })
+        );
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+    // Cleanup subscription on unmount
+    return () => {
+      unsubscribe1();
+      unsubscribe2();
+      unsubscribe3();
+    };
   }, []);
 
-  const fireTest = async () => {
-    
-    try {
 
-      let categoryArray = [], dietaryArray = [], foodArray = [];
+  const updateState = (obj) => {
 
-      // 1 API REQUEST
-      console.debug("FETCHING DATA FROM FIREBASE");
-      const foods = await firestore.collection("foods").limit(25).get();
+    const { categories, foods, dietaryconditions} = obj;
 
-      // 2 API REQUESTS
-      console.debug("FETCHING DATA FROM FIREBASE");
-      const categories = await firestore.collection("categories").limit(25).get();
-
-      // 3 API REQUESTS ~ 75 items
-      console.debug("FETCHING DATA FROM FIREBASE");
-      const dietaryconditions = await firestore.collection("dietaryconditions").limit(25).get();
-
-     // LOGIC
-      categories.forEach((category) => { 
-        const { name } = category.data(); 
-        if(!name) return; // REQUIRED name value to avoid bad practice of setting document ID's directly
-        categoryArray.push({name, path: category.ref.path});
-      });
-
-      dietaryconditions.forEach((condition) => {
-        const { name } = condition.data();
-        if(!name) return; // REQUIRED name value to avoid bad practice of setting document ID's directly
-        dietaryArray.push({name, path: condition.ref.path});
-      });
-
-      foods.forEach((food) => { 
-        const { name, intolerance, category } = food.data();
-        let arr = [], arr2 = [""];
-        if(!name) return; 
-        if(intolerance) intolerance.forEach((condition) => { 
-          const found = dietaryArray.find(({path}) => path === condition.path)
-          if(found) arr.push(found.name);
-        });
-        if(category) { 
-          const found = categoryArray.find(({path}) => path === category.path); 
-          if(found) arr2[0] = found.name;
-        };
-
-        foodArray.push({name, intolerance: arr, category: arr2[0]});
-
-      });
-
-      console.log(foodArray);
-
-    } catch (error) {
-      console.log(error)
-    }
+    if(categories) setCategories((prevState) => [ ...reducer(prevState, categories)]);
+    if(foods) setFoods((prevState) => [ ...reducer(prevState, foods)]);
+    if(dietaryconditions) setDietaryConditions((prevState) => [ ...reducer(prevState, dietaryconditions)]);
+ 
   }
 
-  const data = [
-    "Vegetarian",
-    "Vegan",
-    "Islam",
-    "Sikh",
-    "Hindu",
-    "Jewish",
-    "Pregnancy",
-    "Elderly",
-    "Breastfeeding",
-    "6-12 Months",
-    "12-24 months",
-    "5-12 years",
-    "Teenagers (12+)",
-    "Coeliac disease",
-    "Peanut allergy",
-    "Lactose intolerant",
-    "Caffeine intolerant",
-    "Celery allergy",
-    "Fish allergy",
-    "Shellfish allergy",
-    "Egg allergy",
-    "Milk allergy",
-    "Lupin allergy",
-    "Sesame allergy",
-    "Mustard allergy",
-    "Soya allergy",
-    "Sulphur dioxide (sulphites) allergy",
-    "Diabetes",
-    "Cardiovascular Disease (CVD)",
-  ];
+  const reducer = (prevState, array) => {
 
-  const data2 = [
-    "Celery",
-    "Crustaceans / shellfish",
-    "Eggs",
-    "Fish",
-    "Gluten",
-    "Lupin",
-    "Cow's Milk",
-    "Molluscs",
-    "Mustard",
-    "Peanuts",
-    "Sesame",
-    "Soya",
-    "Sulphites",
-    "Tree nuts",
-    "Halal meat",
-    "Alcohol",
-  ];
+    let newArray = array.data.filter((condition) => condition.name);
+
+    return [...prevState.map((item) => !!newArray.length ? 
+      item.path === newArray[0].path ? 
+        newArray[0]
+      : item
+    : item
+    ), ...newArray];
+  }
 
   return (
     <div className="swiper-container">
@@ -175,17 +147,27 @@ export default function Welcome() {
                 type="button"
                 variant="success"
                 onClick={() => mySwiper.slideNext()}
-                style={{width: "25%", minWidth: "140px", maxHeight: "65px"}}>
+                style={{ width: "25%", minWidth: "140px", maxHeight: "65px" }}
+              >
                 Next
               </Button>
             </Card.Body>
           </Card>
-          <br/>
+          <br />
         </div>
         <div className="swiper-slide">
           <Card>
-            <div style={{backgroundImage: `url(${mainLogo2})`, height: "50vh", backgroundSize: "cover",  }} />
-            <Card.Body className="mt-2 text-center" style={{ maxWidth: "800px" }}>
+            <div
+              style={{
+                backgroundImage: `url(${mainLogo2})`,
+                height: "50vh",
+                backgroundSize: "cover",
+              }}
+            />
+            <Card.Body
+              className="mt-2 text-center"
+              style={{ maxWidth: "800px" }}
+            >
               <h2 className="title">Let's Get Started</h2>
               <Card.Text style={{ padding: "5px" }}>
                 Create a dietary profile for foods you can't eat.
@@ -195,12 +177,13 @@ export default function Welcome() {
                 type="button"
                 variant="success"
                 onClick={() => mySwiper.slideNext()}
-                style={{width: "25%", minWidth: "140px", maxHeight: "65px"}}>
+                style={{ width: "25%", minWidth: "140px", maxHeight: "65px" }}
+              >
                 Next
               </Button>
             </Card.Body>
           </Card>
-          <br/>
+          <br />
         </div>
         <div className="swiper-slide">
           <Container fluid style={{ padding: "20px" }}>
@@ -212,38 +195,68 @@ export default function Welcome() {
               >
                 My Food Profile
               </Card.Header>
-              <Card.Body className="text-center" style={{paddingBottom: "0px", maxWidth: "800px"}}>
-                Tell us about which foods you can't eat. 
+              <Card.Body
+                className="text-center"
+                style={{ paddingBottom: "0px", maxWidth: "800px" }}
+              >
+                Tell us about which foods you can't eat.
                 <Form className="m-sm-4">
                   <Form.Row className="checkboxgroup">
-                    {data2.map((item, index) => (
-                      <Col xs={6} sm={6} md={6} lg={4}>
-                        <Form.Group
-                          key={index}
-                          style={{ marginBottom: ".75rem" }}
-                        >
-                          <Form.Check type="checkbox" label={item} />
-                        </Form.Group>
-                      </Col>
+                    
+                  <Accordion defaultActiveKey="0">
+                    {categories.map((item, index) => (
+                        <Card>
+                          <Accordion.Toggle as={Card.Header} eventKey={index}>
+                          {item.name}
+                          </Accordion.Toggle>
+                            <Accordion.Collapse eventKey={index}>
+                          <Card.Body>
+                            {foods.map((item2, index2) => item2.name === "Celery" && (
+                              <Col key={index2} xs={6} sm={6} md={6} lg={4}>
+                                <Form.Group style={{ marginBottom: ".75rem" }}>
+                                <Form.Check type="checkbox" label={item2.name} />
+                                </Form.Group>
+                              </Col>
+                            ))}
+                          </Card.Body>
+                        </Accordion.Collapse>
+                     </Card> 
                     ))}
+                  </Accordion>
+                      
+
                   </Form.Row>
                 </Form>
               </Card.Body>
-              <Card.Footer style={{backgroundColor: "rgba(0,0,0,0)", border: "none", padding: "0px"}}>
-                <Container className="text-center" style={{maxWidth: "400px"}}>
-                       <Button
-                        onClick={() => mySwiper.slideNext()}
-                        size="lg"
-                        type="button"
-                        variant="success"
-                        className="btn btn-success btn-lg"
-                        style={{width: "25%", minWidth: "140px", maxHeight: "65px"}}>
-                        Next
-                      </Button>
+              <Card.Footer
+                style={{
+                  backgroundColor: "rgba(0,0,0,0)",
+                  border: "none",
+                  padding: "0px",
+                }}
+              >
+                <Container
+                  className="text-center"
+                  style={{ maxWidth: "400px" }}
+                >
+                  <Button
+                    onClick={() => mySwiper.slideNext()}
+                    size="lg"
+                    type="button"
+                    variant="success"
+                    className="btn btn-success btn-lg"
+                    style={{
+                      width: "25%",
+                      minWidth: "140px",
+                      maxHeight: "65px",
+                    }}
+                  >
+                    Next
+                  </Button>
                 </Container>
               </Card.Footer>
             </Card>
-            <br/>
+            <br />
           </Container>
         </div>
         <div className="swiper-slide">
@@ -256,40 +269,56 @@ export default function Welcome() {
               >
                 Dietary Conditions
               </Card.Header>
-              <Card.Body className="text-center" style={{paddingBottom: "0px", maxWidth: "800px"}}>
+              <Card.Body
+                className="text-center"
+                style={{ paddingBottom: "0px", maxWidth: "800px" }}
+              >
                 Tell us about your dietary conditions.
                 <Form className="m-sm-4">
                   <Form.Row className="checkboxgroup">
-                    {data.map((item, index) => (
-                      <Col xs={6} sm={6} md={6} lg={4}>
-                        <Form.Group
-                          key={index}
-                          style={{ marginBottom: ".75rem" }}
-                        >
-                          <Form.Check type="checkbox" label={item} />
-                        </Form.Group>
-                      </Col>
-                    ))}
+                  {dietaryConditions.map((item, index) => (
+                    <Col key={index} xs={6} sm={6} md={6} lg={4}>
+                      <Form.Group
+                        style={{ marginBottom: ".75rem" }}
+                      >
+                        <Form.Check
+                          type="checkbox" label={item.name}
+                        />
+                      </Form.Group>
+                    </Col>
+                   ))}
                   </Form.Row>
                 </Form>
               </Card.Body>
-              <Card.Footer style={{backgroundColor: "rgba(0,0,0,0)", border: "none", padding: "0px"}}>
-                <Container className="text-center" style={{maxWidth: "400px"}}>
-
-                       <Button
-                        onClick={() => history.go(0)}
-                        size="lg"
-                        type="button"
-                        variant="success"
-                        className="btn btn-success btn-lg"
-                        style={{width: "25%", minWidth: "140px", maxHeight: "65px"}}>
-                        Finish
-                      </Button>
-
+              <Card.Footer
+                style={{
+                  backgroundColor: "rgba(0,0,0,0)",
+                  border: "none",
+                  padding: "0px",
+                }}
+              >
+                <Container
+                  className="text-center"
+                  style={{ maxWidth: "400px" }}
+                >
+                  <Button
+                    onClick={() => history.go(0)}
+                    size="lg"
+                    type="button"
+                    variant="success"
+                    className="btn btn-success btn-lg"
+                    style={{
+                      width: "25%",
+                      minWidth: "140px",
+                      maxHeight: "65px",
+                    }}
+                  >
+                    Finish
+                  </Button>
                 </Container>
               </Card.Footer>
             </Card>
-            <br/>
+            <br />
           </Container>
         </div>
       </div>
